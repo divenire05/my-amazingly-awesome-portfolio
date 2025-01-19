797dbdb3dd4b9874dcba007b5461a5726de0f905
Vedant Badoni <vedant.badoni@outlook.com>
2025-01-19 14:09:32 -0500

    finishing first part of main component

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -13,7 +13,7 @@
     </h1>
     <div class="sm:flex items-center gap-4 hidden">
         {#each tabs as tab, index}
-            <a href={tab.link} class="duration-200 hover:text-viol-400">
+            <a href={tab.link} class="duration-200 hover:text-violet-400">
                 <p>{tab.name}</p>
             </a>
         {/each}
